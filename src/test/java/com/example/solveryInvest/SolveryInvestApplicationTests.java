572b42fd857593b9378d713cd077136183da809d
IvanM13th <bmchek13@gmail.com>
2023-10-05 16:00:04 +0500

    first commit

---
+++ src/test/java/com/example/solveryInvest/SolveryInvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.solveryInvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolveryInvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
