be7c46797b018650fd96f326573bc3caca199716
IvanM13th <bmchek13@gmail.com>
2024-02-17 18:23:26 +0500

    test example added

---
+++ stocks/src/test/java/solveryinvest/stocks/service/impl/BalanceServiceImplTest.java
@@ -0,0 +1,17 @@
+package solveryinvest.stocks.service.impl;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class BalanceServiceImplTest {
+
+    @Test
+    void createBalance() {
+        var int1 = 123;
+        assertEquals(int1, 123);
+    }
+}
